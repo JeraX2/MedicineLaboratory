c1e5796b286b5c2c1a100c561de13d78e239b0e6
Jera123 <12572099+jera123@user.noreply.gitee.com>
2025-04-06 23:14:32 +0800

    解决跨域问题

--- src/main/java/com/example/monitordashboardbackend/demos/controller/MonitorController.java
+++ src/main/java/com/example/monitordashboardbackend/demos/controller/MonitorController.java
@@ -5,6 +5,7 @@ import com.example.monitordashboardbackend.demos.entity.Message;
 import com.example.monitordashboardbackend.demos.entity.UsageData;
 import com.example.monitordashboardbackend.demos.service.MonitorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class MonitorController {
     @Autowired
     private MonitorService monitorService;
